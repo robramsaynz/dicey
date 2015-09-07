b68870b33adc9b54710dcbf6d19b73fbdf45e861
Rob Ramsay <robert.ramsay.nz@gmail.com>
2015-09-08 10:47:08 +1200

    parse file for dictionary and use to print word

--- dicey.exs
+++ dicey.exs
@@ -1,11 +1,23 @@
 
-dicey = fn ->
+dice_num = fn ->
+  # create a list of integers [1..6], and convert to a string
+
   :crypto.rand_bytes(5)
   |> :binary.bin_to_list
-  |> Enum.map( &( round &1 / 256 * 7 ) )
+  |> Enum.map( &( round(&1 / 256 * 5) + 1 ) )
   |> Enum.join
 end
 
-IO.puts dicey.()
+{:ok, file_text} = File.read("diceware.wordlist.asc")
+lines = String.split(file_text, ~r{\R})
+dictionary_lines = Enum.filter(lines, &( &1 =~ ~r/^\d{5}/ ))
 
+key_list = Enum.map(dictionary_lines, fn line ->
+	[k, v] = String.split(line, "\t")
+	k = String.to_atom(k)
+	{k, v}
+end)
 
+num = dice_num.()
+key = String.to_atom(num)
+IO.puts( num <> " -> " <> key_list[key] )
